b6a962ef98a6e9edfe1cb93a2ea69141cf2bae5e
Marius Seiter <101047993+MetaMarius@users.noreply.github.com>
2025-02-09 18:45:58 +0100

    updated setup file (long_description)

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
 
 setup(
     name='toscinterface',
@@ -9,6 +12,7 @@ setup(
     author='Marius Seiter',
     author_email='mariuss98@hotmail.de',
     description='A simple package to receive OSC messages sent by TouchOSC',
+    long_description=long_description,
     license='MIT',
     python_requires='>=3.10'
 
